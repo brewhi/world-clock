6728e74c58da7ab2782512a5717a7dcc22a8dce9
brewhi <126518688+brewhi@users.noreply.github.com>
2023-04-14 12:51:00 -0400

    added sydney font page element

--- src/index.js
+++ src/index.js
@@ -22,6 +22,16 @@ function updateTime() {
       "hh:mm:ss[<small>]A[</small>]"
     );
   }
+  let sydneyElement = document.querySelector("#sydney");
+  if (sydneyElement) {
+    let sydneyDateElement = sydneyElement.querySelector(".date");
+    let sydneyTimeElement = sydneyElement.querySelector(".time");
+    let sydneyTime = moment().tz("Australia/Sydney");
+    sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do, YYYY");
+    sydneyTimeElement.innerHTML = sydneyTime.format(
+      "hh:mm:ss[<small>]A[</small>]"
+    );
+  }
 }
 
 updateTime();
